8fcce08f3c3a5418679da7959cab5d2e934d7c37
Piyush Kumar <piyushkumar8575@gmail.com>
2025-07-28 16:41:36 +0530

    Final Commit after local Testing

--- src/main/java/com/piyush/cp/controller/StudentController.java
+++ src/main/java/com/piyush/cp/controller/StudentController.java
@@ -1,5 +1,39 @@
 package com.piyush.cp.controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.piyush.cp.model.Student;
+import com.piyush.cp.repository.StudentRepository;
+
+@RestController
+@RequestMapping("/students")
 public class StudentController {
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@PostMapping
+	public String saveStudent(@RequestBody Student student) {
+		try {
+			studentRepository.saveStudent(student);
+			return "Student Saved Successfully";
+		} catch (Exception e) {
+			return "Error : "+ e.getMessage();
+		}
+	}
+	
+	@GetMapping
+	public List<Student> getStudents() throws InterruptedException, SQLException{
+		return studentRepository.findAll();
+	}
 
 }
--- src/main/java/com/piyush/cp/repository/StudentRepository.java
+++ src/main/java/com/piyush/cp/repository/StudentRepository.java
@@ -1,5 +1,65 @@
 package com.piyush.cp.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.piyush.cp.config.CustomConnectionPool;
+import com.piyush.cp.model.Student;
+
+import jakarta.annotation.PostConstruct;
+
+@Repository
 public class StudentRepository {
+	
+	private CustomConnectionPool connectionPool;
+	
+	@PostConstruct
+	public void init() {
+		try {
+			connectionPool = new CustomConnectionPool("jdbc:h2:mem:testdb", "sa", "", 5);
+			
+			// create table at the start up
+			try(Connection connection = connectionPool.getConnection()) {
+				Statement stmt = connection.createStatement();
+				stmt.executeUpdate("CREATE TABLE IF NOT EXISTS student (id INT PRIMARY KEY, name VARCHAR(225), department VARCHAR(225))");
+				connectionPool.releaseConnection(connection);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void saveStudent(Student student) throws Exception{
+		Connection connection = connectionPool.getConnection();
+		String sql = "INSERT INTO student (id, name, department) VALUES (?, ?, ?)";
+		PreparedStatement ps = connection.prepareStatement(sql);
+		ps.setInt(1, student.getId());
+		ps.setString(2, student.getName());
+		ps.setString(3, student.getDepartment());
+		ps.executeUpdate();
+		connectionPool.releaseConnection(connection);
+	}
+	
+	public List<Student> findAll() throws InterruptedException, SQLException{
+		Connection connection = connectionPool.getConnection();
+		Statement stmt = connection.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM student");
+		
+		List<Student> students = new ArrayList<>();
+		while(rs.next()) {
+			students.add(new Student(rs.getInt("id"), rs.getString("name"), rs.getString("department")));
+		}
+		connectionPool.releaseConnection(connection);
+		return students;
+	}
 
 }
